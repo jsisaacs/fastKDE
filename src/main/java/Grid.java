6523cd93c728b559376e40457a1ae5348107d7e6
Josh Isaacson <jsisaacs@iu.edu>
2018-03-30 04:55:07 -0400

    finished the Array class, working on the optimize gridsize problem

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -1,2 +1,20 @@
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.ops.transforms.Transforms;
+
 public class Grid {
+  private int gridSizeX;
+  private int getGridSizeY;
+  private INDArray grid;
+
+  public Grid(Array array) {
+    //TODO
+  }
+
+  public Grid(int gridSizeX, int getGridSizeY, Array array) {
+    //TODO
+
+    this.gridSizeX = gridSizeX;
+    this.getGridSizeY = getGridSizeY;
+  }
 }
--- src/test/java/FastKdeTest.java
+++ src/test/java/FastKdeTest.java
@@ -1,8 +1,18 @@
 import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 public class FastKdeTest {
   @Test
-  public void arraySetupTest() {
+  public void basicArrayTest() throws Exception {
+    INDArray x = Nd4j.create(1, 2);
+    INDArray y = Nd4j.create(1, 2);
+    Array array = new Array(x, y);
+    Grid grid = new Grid(array);
+  }
+
+  @Test
+  public void complexArrayTest() {
 
   }
 }
